ed8cdf0e54f4a6fda14163b53c9d38a346bda9e4
Oğuzhan <oguzhnural@gmail.com>
2024-10-20 11:06:23 +0300

    compilation error for typescript

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,57 @@
+// import { ApolloServer } from "@apollo/server";
+// import { expressMiddleware } from "@apollo/server/express4";
+// import express from "express";
+// import cors from "cors";
+// import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+// import  json  from "body-parser";
+// import mongoose from "mongoose";
+// import http from "http";
+// import typeDefs from "./schemas/schema.js";
+// import userResolvers from "./resolvers/userResolvers.js";
+// import { MONGODB_URI } from "./config/environment.js";
+
+// interface MyContext {
+//     token?: string;
+// }
+
+// async function startApolloServer() {
+//     const app = express();
+//     const httpServer = http.createServer(app);
+
+//     const server = new ApolloServer<MyContext>({
+//         typeDefs,
+//         resolvers: userResolvers,
+//         plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+//     });
+
+//     await server.start();
+
+//     try {
+//         await mongoose.connect(MONGODB_URI!);
+//         console.log("Connected to MongoDB");
+//     } catch (error) {
+//         console.log('Error connecting to MongoDB', error);
+//         process.exit(1);
+//     }
+
+//     app.use(
+//         '/graphql',
+//         cors<cors.CorsRequest>(),
+//         json(),
+//         expressMiddleware(server, {
+//             context: async ( { req } ): Promise<MyContext> => ( { token: req.headers.token as string}),
+//         })
+//     );
+
+//     await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
+//     console.log(`🚀 Server ready at httt://localhost:4000/graphq`);
+// }
+
+// startApolloServer().catch((err) => console.error('Error starting server', err));
+
 import { ApolloServer } from "@apollo/server";
-import { expressMiddleware } from "@apollo/server/express4";
-import express from "express";
-import cors from "cors";
-import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
-import  json  from "body-parser";
+import { startStandaloneServer } from "@apollo/server/standalone";
 import mongoose from "mongoose";
-import http from "http";
 import typeDefs from "./schemas/schema.js";
 import userResolvers from "./resolvers/userResolvers.js";
 import { MONGODB_URI } from "./config/environment.js";
@@ -15,40 +61,33 @@ interface MyContext {
 }
 
 async function startApolloServer() {
-    const app = express();
-    const httpServer = http.createServer(app);
-
+    // Apollo Server kurulumunu gerçekleştiriyoruz
     const server = new ApolloServer<MyContext>({
         typeDefs,
         resolvers: userResolvers,
-        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
     });
 
-    await server.start();
-
+    // MongoDB bağlantısı kuruluyor
     try {
         await mongoose.connect(MONGODB_URI!);
         console.log("Connected to MongoDB");
     } catch (error) {
-        console.log('Error connecting to MongoDB', error);
+        console.error('Error connecting to MongoDB', error);
         process.exit(1);
     }
 
-    app.use(
-        '/graphql',
-        cors<cors.CorsRequest>(),
-        json(),
-        expressMiddleware(server, {
-            context: async ( { req } ): Promise<MyContext> => ( { token: req.headers.token as string}),
-        })
-    );
+    // Apollo Server'ı başlatıp URL'i döndürüp sunucuyu ayağa kaldırıyoruz
+    const { url } = await startStandaloneServer(server, {
+        context: async ({ req }): Promise<MyContext> => {
+            return { token: req.headers.token as string };
+        },
+        listen: { port: 4001 }, // 4000 portunda dinleyecek
+    });
 
-    await new Promise<void>((resolve) => httpServer.listen({ port: 4000 }, resolve));
-    console.log(`🚀 Server ready at httt://localhost:4000/graphq`);
+    console.log(`🚀 Server ready at ${url}`);
 }
 
 startApolloServer().catch((err) => console.error('Error starting server', err));
-
 
 
 
